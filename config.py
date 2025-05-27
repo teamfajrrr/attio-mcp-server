ec4303a2c9cd03d65e9fa364a6ae48e245dd4866
teamfajrrr <team@fajrrr.com>
2025-05-27 11:07:00 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
 
 BASE_URL = os.getenv("BASE_URL", "https://api.attio.com")
 API_KEY = os.getenv("API_KEY")
-PORT = os.getenv("PORT", 8000)
+PORT = os.environ.get("PORT", "8080")  # This returns a string
 TRANSPORT = os.getenv("TRANSPORT", "sse")
